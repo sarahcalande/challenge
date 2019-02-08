7f59262bc63b9b7b1774e7e70542538077da891c
sarahcalande <scalande@yahoo.com>
2019-02-07 21:51:09 -0500

    done, should replace duplicates

--- FileReaderindex.js
+++ FileReaderindex.js
@@ -35,7 +35,6 @@ function loadHandler(event) {
 	processData(csv);
 }
 
-
 //format CSV rows to arrays and splits with commas
 function processData(csv) {
     var allTextLines = csv.split(/\r\n|\n/);
@@ -43,32 +42,52 @@ function processData(csv) {
     while (allTextLines.length) {
         lines.push(allTextLines.shift().split(','));
       }
+    }
+
 
 
 //for each array/row, identify indices to make into data objects
 //With console.log of lines, I can see all First names were 0 index, etc.
-var i;
-for (i = 0; i < lines.length; i++) {
+
+function parseInfo(){
+for (i = 0; i < lines.length; i++){
+  if (i !== 19){
     let firstName = (lines[i][0])
     let lastName = (lines[i][1])
     let email = (lines[i][2]).toLowerCase().replace('%3c','').replace('&gt;','')
     //some emails had weird formatting with these characters included. I also put
     //all emails to lowercase because postman showed that as the format in the GET/POST requests
     let birthday = new Date(lines[i][3])
-    //1- is a problem in phones, can't have any other characters according to postman.
-    // Looks like format is +1 with 12 chars total
-
-    let homePhone, workPhone;
-
-    if (lines[i][4] != null) {
+    // if (lines[i][4] != null) {
       homePhone = '+1' + (lines[i][4]).split('').map(n => n.replace('-', '')).join('')
-    }
-    if (lines[i][5] != null) {
+    // }
+    // if (lines[i][5] != null) {
       workPhone = '+1' + (lines[i][5]).split('').map(n => n.replace('-', '')).join('')
+    // }
+    let customerType = (lines[i][6])
+
+    createObjects(firstName, lastName, homePhone, email, workPhone, customerType, birthday)
+}
+//The guy in row 19 had a Jr. at the end of his name which was adding an extra column,
+//so his indices were off by one.
+else if (i === 19){
+  let firstName = lines[19][0]
+  let lastName = lines[19][1]+lines[19][2]
+  let email = lines[19][3]
+  let birthday = new Date(lines[19][4])
+  let homePhone = '+1' + (lines[19][5]).split('').map(n => n.replace('-', '')).join('')
+  let workPhone = '+1' + (lines[19][6]).split('').map(n => n.replace('-', '')).join('')
+  let customerType = (lines[19][7])
+  //
+  createObjects(firstName, lastName, homePhone, email, workPhone, customerType, birthday)
     }
+  }
+}
 
-    const phones = []
 
+
+function createObjects(firstName, lastName, homePhone, email, workPhone, customerType, birthday){
+const phones = []
 if (homePhone !== '+1') {
   phones.push({
     "phone": homePhone,
@@ -86,6 +105,19 @@ if (workPhone !== '+1' ) {
   )
 }
 
+let emails = []
+if (email !== ""){
+  emails.push({
+    "email": email,
+    "verified": false,
+    "type": "home"
+  })
+}
+
+
+postRequest(firstName, lastName, emails, phones, birthday, customerType)
+
+}
 
   //
   //
@@ -101,33 +133,14 @@ if (workPhone !== '+1' ) {
 
 
 
-    let customerType = (lines[i][6])
-
-
-// if the string is empty, have a way to send data
-if (firstName === null){
-  firstName = "not available"
-}
-
-if (email === null){
-  email = "not available"
-}
-
-if (customerType === null){
-  customerType = "not available"
-}
-
 //left as verified: false because all postman responses were this way. These parts,
 //including birthday, were not included in docs but were part of postman requests.
+
+
+function postRequest(firstName, lastName, emails, phones, birthday, customerType){
 let sendData = JSON.stringify({
   "name": `${firstName} ${lastName}`,
-  "emails": [
-    {
-      "email": email,
-      "verified": false,
-      "type": "home"
-    }
-  ],
+  "emails": emails,
   "phones": phones,
   "urls": [
     {
@@ -149,41 +162,23 @@ let sendData = JSON.stringify({
   ],
 })
 
-console.log(sendData)
-
-
-
 // MAKE POST REQUEST (derived from Kustomer Docs, normal Kustomer Docs request
 // was not working because of CORS.):
-
 var xhr = new XMLHttpRequest();
-
 //CORS would not allow POST request with xhr.withCredentials set to true.
 xhr.withCredentials = false;
-
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
     console.log(this.responseText);
   }
 });
-
-
-//POST Request also did not work to the normal URL so I implemented CORS anywhere to bypass CORS.
+// POST Request also did not work to the normal URL so I implemented CORS anywhere to bypass CORS.
 xhr.open("POST", "https://cors-anywhere.herokuapp.com/https://api.kustomerapp.com/v1/customers");
 xhr.setRequestHeader("authorization", `Bearer ${token}`);
 xhr.setRequestHeader("content-type", "application/json");
-
-
 xhr.send(sendData);
+  }
 
-
-}
-
-
-
-
-
-}
 
 
 
@@ -193,18 +188,3 @@ function errorHandler(evt) {
 		alert("Cannot read file !");
 	}
 }
-
-
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-//
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === this.DONE) {
-//     console.log(this.responseText);
-//   }
-// });
-//
-// xhr.open("GET", "https://api.kustomerapp.com/v1/customers");
-// xhr.setRequestHeader("authorization", `Bearer ${token}`);
-// xhr.setRequestHeader("content-type", "application/json");
